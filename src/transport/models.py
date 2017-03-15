94ff14533389957ae9b77ac49854b16729936b80
Neha <neha10gupta97@gmail.com>
2017-03-16 02:39:46 +0530

    login

--- src/transport/models.py
+++ src/transport/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
+class user_details(models.Model):
     username=models.CharField(max_length=120)
     password=models.CharField(max_length=120)
 
--- src/transport/views.py
+++ src/transport/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import user
+from django.http import HttpResponse,Http404
+from django.contrib.auth import authenticate
+from .models import user_details
 
 # Create your views here.
 def transport_home(request):
+    username = request.GET['username']
+    password = request.GET['password']
+    user = authenticate(username=username, password=password)
+    if user is None:
+        raise Http404
+    else :
+        if user.is_active:
+            
     return render(request, "home.html")
