43a8e0b9448f19453b1e35cd60a10daddfd280c0
Neha <neha10gupta97@gmail.com>
2017-03-16 16:54:00 +0530

    login final

--- src/its/settings.py
+++ src/its/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE_CLASSES = [
 ROOT_URLCONF = 'its.urls'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+]
 
 TEMPLATES = [
     {
