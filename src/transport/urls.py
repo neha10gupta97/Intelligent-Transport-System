f71ded22795f1e76b1ece5cb8483c9789bbf10b4
Neha <neha10gupta97@gmail.com>
2017-03-19 14:59:26 +0530

    login.html

--- src/transport/urls.py
+++ src/transport/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views
 
 
 urlpatterns=[
-    url(r'^login/$',views.login,{'template_name': 'login.html'}),
+    url(r'^login/$',views.login,{'template_name': 'Login.html'}),
     url(r'^register/$',"transport.views.register_page"),
     url(r'^register/$',"transport.views.logout_page"),
     url(r'^$',"transport.views.home")
